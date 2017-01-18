bfdf829db9a6b5106b41eaa1ddea53cb8e9628d1
Andy Stef <Andy.stef.2512@gmail.com>
2017-01-18 21:34:53 +0200

    added buttons to navigation controller and added dynamic sizing to cells

--- Twitter/HomeDatasourceViewController.swift
+++ Twitter/HomeDatasourceViewController.swift
@@ -13,6 +13,43 @@ class HomeDatasourceViewController: DatasourceController {
         super.viewDidLoad()
 
         datasource = HomeDatasource()
+        setupNavigationBarItems()
+        collectionView?.backgroundColor = UIColor.lightGray
+    }
+
+    private func setupNavigationBarItems() {
+        setupLeftNavItems()
+        setupRightNavItems()
+        setupRemainingThings()
+    }
+
+    private func setupRemainingThings() {
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "twitter-logo"))
+        logoImageView.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        logoImageView.contentMode = .scaleAspectFit
+        navigationItem.titleView = logoImageView
+    }
+
+    private func setupLeftNavItems() {
+        let followButton = UIButton(type: .system)
+        followButton.setImage(#imageLiteral(resourceName: "follow"), for: .normal)
+        followButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: followButton)
+    }
+
+    private func setupRightNavItems() {
+        let searchButton = UIButton(type: .system)
+        searchButton.setImage(#imageLiteral(resourceName: "search-icon"), for: .normal)
+        searchButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+
+        let composeButton = UIButton(type: .system)
+        composeButton.setImage(#imageLiteral(resourceName: "compose-icon"), for: .normal)
+        composeButton.frame = CGRect(x: 0, y: 0, width: 34, height: 34)
+
+        navigationItem.rightBarButtonItems = [UIBarButtonItem(customView: composeButton), UIBarButtonItem(customView: searchButton)]
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
@@ -20,10 +57,26 @@ class HomeDatasourceViewController: DatasourceController {
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-        return CGSize(width: view.frame.width, height: 58)
+        return CGSize(width: view.frame.width, height: 50)
     }
 
     override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 150)
+        if let user = datasource?.item(indexPath) as? User {
+            let approximateWidthOfBioTextView = view.frame.width - 12 - 50 - 12 - 2
+
+            //height needs to be very large
+            let size = CGSize(width: approximateWidthOfBioTextView, height: 1000)
+            let attributes = [NSFontAttributeName: UIFont.systemFont(ofSize: 15)]
+            let estimatedFrame = NSString(string: user.bioText).boundingRect(with: size, options: .usesLineFragmentOrigin, attributes: attributes, context: nil)
+
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 66)
+        }
+
+        return CGSize(width: view.frame.width, height: 200)
+    }
+
+    //MARK: Used to remove gap between cells
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
 }
